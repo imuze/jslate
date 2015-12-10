c5ffbbc040da55b280bad135ead73873e5c10cff
Geoffroy <nycgdf@gmail.com>
2015-12-10 00:50:01 -0800

    So this is JSlate.

---
+++ JSlate/JSlate.js
@@ -0,0 +1,135 @@
+/**
+ * Created by Geoffroy on 10/4/2015.
+ */
+var jsf = require('json-schema-faker');
+var dir = require('node-dir');
+var fs = require('fs');
+var jsrender = require('node-jsrender');
+var path = require('path');
+var titlize = require('to-title-case');
+
+var domain = "https://imuze.io"
+var indexDotMd = '';
+
+
+jsrender.loadFileSync('#section', './templates/section.jsr');
+jsrender.loadFileSync('#moduleIntro', './templates/module-intro.jsr');
+jsrender.loadFileSync('#mainIntro', './templates/main-intro.jsr');
+
+function jslate() {
+        indexDotMd += jsrender.render['#mainIntro']();
+
+    indexDotMd += generate_modules();
+
+    fs.writeFile(__dirname + "/source/index.md", indexDotMd, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("index.md file has been saved !!");
+    });
+
+}
+
+function dirList(dir) {
+  return fs.readdirSync(dir).reduce(function(list, file) {
+    var name = path.join(dir, file);
+    var isDir = fs.statSync(name).isDirectory();
+    return list.concat(isDir ? name : []);
+  }, []);
+}
+
+function generate_modules() {
+    var self = this;
+    self.result = "";
+
+    self.generate = function (dir) {
+        self.result += generate_module(dir);
+    };
+    dirList(__dirname + "/schemas").forEach(generate);
+
+
+
+    console.log(self.result);
+    return self.result;
+}
+
+function generate_module(module_dir) {
+    var self = this;
+    self.result = "";
+    self.result += generate_module_intro(module_dir);
+
+    self.readFileCallback = function (input_schema_path) {
+        console.log("ss");
+
+        console.log(input_schema_path);
+        if (input_schema_path.match("\.input\.json$")) {
+            console.log(input_schema_path);
+            var input_schema_path = input_schema_path;
+            var output_schema_path = output_path(input_schema_path);
+
+            var input_example_path = example_path(input_schema_path);
+            var output_example_path = example_path(output_schema_path);
+
+            var input_schema = file2json(input_schema_path);
+            var output_schema = file2json(output_schema_path);
+
+            var input_example = generate_example(input_example_path, input_schema);
+            var output_example = generate_example(output_example_path, output_schema);
+
+            var markdown = generate_markdown(input_schema, output_example);
+            self.result += markdown;
+        }
+
+    };
+
+    fs.readdirSync(module_dir).map(function (dvar) {
+        return path.join(module_dir, dvar)
+    }).forEach(readFileCallback);
+    return self.result;
+}
+
+function generate_module_intro(module_dir) {
+    var res = jsrender.render['#moduleIntro']({
+        module_name: titlize(path.basename(module_dir)),
+    });
+
+    return res;
+}
+
+function generate_markdown(input_schema, output_example) {
+    //    console.log(jsrender);
+    var res = jsrender.render['#section']({
+        section_name: input_schema.title,
+        json_example: JSON.stringify(output_example, null, 4),
+        domain: domain || "",
+        schema: input_schema
+    });
+    //    console.log(res);
+    return res;
+}
+
+function generate_example(exampleFileName, schema) {
+    var res = new jsf(schema);
+    console.log("creating"+exampleFileName);
+    fs.writeFile(exampleFileName, JSON.stringify(res, null, 4), 'utf-8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+    return res;
+}
+
+function example_path(schema_path) {
+    return schema_path.replace("schemas", "examples");
+}
+
+function output_path(input_schema_path) {
+    return input_schema_path.replace(".input.json", ".output.json");
+}
+
+function file2json(file_name){
+    return JSON.parse(fs.readFileSync(file_name).toString());
+}
+
+jslate();
---
+++ JSlate/source/index.md
@@ -0,0 +1,75 @@
+---
+title: API Reference
+
+language_tabs:
+  - shell
+  - ruby
+  - python
+
+toc_footers:
+  - <a href='#'>Sign Up for a Developer Key</a>
+  - <a href='http://github.com/tripit/slate'>Documentation Powered by Slate</a>
+
+includes:
+  - errors
+
+search: true
+---
+
+# Introduction
+# User
+
+## createUserInput
+
+```ruby
+require 'kittn'
+
+api = Kittn::APIClient.authorize!('meowmeowmeow')
+api.kittens.get
+```
+
+```python
+import kittn
+
+api = kittn.authorize('meowmeowmeow')
+api.kittens.get()
+```
+
+```shell
+curl "http://example.com/api/kittens"
+  -H "Authorization: meowmeowmeow"
+```
+
+> The above command returns JSON structured like this:
+
+```json
+{
+    "email": "qui",
+    "id": 90007963,
+    "token": "temporibus"
+}
+```
+
+
+
+### HTTP Request
+
+`POST https://imuze.io/users`
+
+### Query Parameters
+
+Parameter | Type
+--------- | -----------
+email | string
+password | string
+name | string
+birthdate | integer
+
+
+<aside class="notice">
+Required properties are : email,password,birthdate
+</aside>
+
+<aside class="success">
+Remember — a happy kitten is an iMuze kitten!
+</aside>
